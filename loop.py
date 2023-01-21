81ce6176f485422d5ebe5ec2b82d2a2bb2023e14
thierno953 <thiernobarry554@gmail.com>
2023-01-21 16:58:01 +0100

    add loop

--- loop.py
+++ loop.py
@@ -4,7 +4,7 @@ names = ["Ahamed", "Annah", "James", "Jamila"]
 for name in names:
     print(name)
     
-print('-------------------------------------')
+# print('-------------------------------------')
 
 # LOOP THROUGH DICTIONARIES
 person = {
@@ -15,8 +15,50 @@ person = {
 
 print(person.items())
 
+# 1
 for key, value in person.items():
     print(f"key: {key} value: {value}")
 
+# # 2 
 # for key in person:
 #     print(f"key: {key} value: {person[key]}")
+
+print('-------------------------------------')
+
+# Exercise
+
+result = 0
+numbers = [1, 2, 3, 5, 6 ,7, 9]
+for number in numbers:
+    result += number
+print(f"Result = {result}")
+
+print('-------------------------------------')
+
+# WHILE LOOP
+
+number = 0
+
+# 3
+number = 0
+while number < 10:
+    print(number)
+    number += 1
+else:
+    print("while loop ended")
+
+
+# 2
+while number < 10:
+    number += 1
+    if number < 5:
+        continue
+    print(number)
+    
+
+# 3
+for n in [1,2,3,4,5, 6, 7]:
+    if n == 5:
+        break
+    print(n)
+
