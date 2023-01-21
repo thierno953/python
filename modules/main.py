6a5dd624cea23007c1f7f484a2bf62e8f15a5203
thierno953 <thiernobarry554@gmail.com>
2023-01-21 17:51:51 +0100

    modules

---
+++ modules/main.py
@@ -0,0 +1,9 @@
+from calculator import add
+from calculator import subtract
+from calculator import divide
+from calculator import multiply
+
+print(add(2, 2))
+print(subtract(2, 2))
+print(divide(2, 2))
+print(multiply(2, 2))
