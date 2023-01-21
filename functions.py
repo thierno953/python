2dbc52b25234bb2c49d526567523551544cc4cd3
thierno953 <thiernobarry554@gmail.com>
2023-01-21 17:26:04 +0100

    add functions

---
+++ functions.py
@@ -0,0 +1,41 @@
+# PARAMETERS AND ARGUMENTS
+
+# def greet(name, age=-1):
+#     print(f"Hello {name} how are you?")
+#     if age >= 0:
+#         print(f"I know your age = {age}")
+
+# greet("Jamila")
+# greet("John", 20)
+
+# print('-----------------------------')
+
+# RETURN VALUES FROM FUNCTIONS
+
+def is_adult(age):
+    return age >= 16
+
+def convertGender(gender="unknown"):
+    if gender.upper() == "M":
+        return "Male"
+    elif gender.upper() == "F":
+        return "Female"
+    else:
+        return f"Gender {gender} is unknown"
+        
+result = is_adult(80)
+print(result)
+
+print(convertGender("F"))
+print(convertGender("f"))
+print(convertGender("M"))
+print(convertGender("m"))
+print(convertGender("hello"))
+
+# print('-----------------------------')
+
+# BUILD IN FUNCTIONS AND IMPORT STATEMENT
+
+
+
+
