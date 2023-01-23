246aea899304f2954e716ddea2024709f3676e44
thierno953 <thiernobarry554@gmail.com>
2023-01-23 19:45:13 +0100

    URL

--- url/main.py
+++ url/main.py
@@ -1,26 +1,29 @@
-from urllib import request
+import requests
 import json
+import pyttsx3
 
 url = "https://official-joke-api.appspot.com/random_ten"
 
-r = request.urlopen(url)
-print(r.getcode())
-data = r.read()
-jsonData = json.loads(data)
+respone = requests.get(url)
+print(respone.status_code)
+
+jsonData = json.loads(respone.text)
 print(jsonData)
 
+
 class Joke:
-    
+
     def __init__(self, setup, punchline) -> None:
         self.setup = setup
         self.punchline = punchline
-    
+
     def __str__(self) -> str:
         return f"setup {self.setup} punchline {self.punchline}"
-        
+
+
 jokes = []
 
-for j in jsonData: 
+for j in jsonData:
     setup = j["setup"]
     punchline = j["punchline"]
     joke = Joke(setup, punchline)
@@ -30,3 +33,7 @@ print(f"Got {len(jokes)} jokes")
 
 for joke in jokes:
     print(joke)
+    pyttsx3.speak("Setup")
+    pyttsx3.speak(joke.setup)
+    pyttsx3.speak("The Punchline")
+    pyttsx3.speak(joke.punchline)
