aede2ada13b3b8e4e9be42639206cfcd95f71b6d
thierno953 <thiernobarry554@gmail.com>
2023-01-23 16:57:14 +0100

    FUNCTIONS

--- functions/main.py
+++ functions/main.py
@@ -1,30 +1,40 @@
 # PARAMETERS AND ARGUMENTS
 
+from math import isqrt
+
+
 def greet(name, age=-1):
     print(f"Hello {name} how are you?")
     if age >= 0:
         print(f"I know your age = {age}")
 
+
 greet("Jamila")
 greet("John", 20)
 
-# print('-----------------------------')
+print('-----------------------------')
 
 # RETURN VALUES FROM FUNCTIONS
 
+
 def is_adult(age):
-    return age >= 16
+    return age >= 18
+
+
+result = is_adult(80)
+print(result)
+
+# print('-----------------------------')
+
 
 def convertGender(gender="unknown"):
     if gender.upper() == "M":
         return "Male"
     elif gender.upper() == "F":
-        return "Female"
+        return "F"
     else:
         return f"Gender {gender} is unknown"
-        
-result = is_adult(80)
-print(result)
+
 
 print(convertGender("F"))
 print(convertGender("f"))
@@ -32,12 +42,9 @@ print(convertGender("M"))
 print(convertGender("m"))
 print(convertGender("hello"))
 
-# print('-----------------------------')
+
+print('-----------------------------')
 
 # BUILD IN FUNCTIONS AND IMPORT STATEMENT
 
-from math import isqrt
 print(isqrt(25))
-
-
-
