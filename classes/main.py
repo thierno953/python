aa4d1c25298518d525d71da696abd24b139967e4
thierno953 <thiernobarry554@gmail.com>
2023-01-23 17:19:58 +0100

    CLASSES

--- classes/main.py
+++ classes/main.py
@@ -1,19 +1,22 @@
 class Phone:
-        
-    def __init__(self, brand, price) -> None:
+
+    def __init__(self, brand, price):
         self.brand = brand
         self.price = price
-    
+
     def call(self, phone_number):
         print(f"{self.brand} is calling {phone_number}")
-    
+
     def __str__(self) -> str:
-        return f"Brand = {self.brand}\nPrice = {self.price}"
-    
+        return f"Brand = {self.brand}\nPrice ={self.price}"
+
+
 iphone = Phone("Iphone 7+", 300)
 samsung = Phone("Samsung S20", 1300)
 
 print(iphone)
-print(iphone)
+print(samsung)
 
+iphone.call("999")
+samsung.call("1000")
 
