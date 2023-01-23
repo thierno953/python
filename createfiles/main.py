41735672c2cc0db6f18e12893f323f1ae189c4f3
thierno953 <thiernobarry554@gmail.com>
2023-01-23 17:35:37 +0100

    FILES

--- createfiles/main.py
+++ createfiles/main.py
@@ -2,11 +2,13 @@
 
 # file = open("./data.csv", "w")
 # file = open("./data.csv", "a")
-
+import os.path
 file = open("./data.csv", "r+")
+
 file.write("id, name, email\n")
 file.write("1, Jamila, jamila@gmail.com\n")
 file.write("2, John, john@gmail.com\n")
+file.write("3, Samir, sami@gmail.com\n")
 
 file.close()
 
@@ -22,17 +24,16 @@ file.close()
 
 print('-----------------------------------')
 
-# with open("./data.csv", "r") as file:
-#     print(file.read())
+with open("./data.csv", "r") as file:
+    print(file.read())
 
 print('-----------------------------------')
 
-import os.path
 
 filename = "./data.csv"
 
 if os.path.isfile(filename):
     with open(filename, "r") as file:
-       print(file.read())
+        print(file.read())
 else:
     print(f"file {filename} does not exist")
