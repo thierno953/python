9177077287f016f7d9fa7a229b9b96ebbaec95ce
thierno953 <thiernobarry554@gmail.com>
2023-01-23 17:22:16 +0100

    DATES & TIMES

--- dates/main.py
+++ dates/main.py
@@ -4,10 +4,15 @@ from datetime import datetime
 from datetime import date
 
 print(datetime.now())
+
 print(datetime.now().year)
+
 print(datetime.now().month)
+
 print(datetime.now().day)
+
 print(date.today())
+
 print(datetime.now().time())
 
 print('----------------------------')
@@ -15,10 +20,15 @@ print('----------------------------')
 # FORMATTING DATES
 
 now = datetime.now()
+
 print(now)
 
 print(now.strftime("%d-%m-%Y %H:%M:%S"))
+
 print(now.strftime("%d-%m-%B %H:%M:%S"))
+
 print(now.strftime("%d-%b-%Y %H:%M:%S"))
+
 print(date.today().strftime("%d-%m-%Y"))
+
 print(date.today().strftime("%d-%B-%Y"))
